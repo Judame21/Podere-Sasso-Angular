7cf5be1ba5641cfaacfce366ba9e32b8dededd7b
Juan David Mendez <juandamendez21@gmail.com>
2024-06-02 17:57:12 -0500

    Añadida seccion de contacto y el enrutamiento de estas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: InicioComponent},
+  { path: 'contacto', title:'Contacto', loadComponent: () => ContactComponent},
+
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+];
